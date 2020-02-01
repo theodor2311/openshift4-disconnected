009686857b766c8a7a1b33f9c26a12fae8bffcaf
Theodor <tili@redhat.com>
2020-02-01 16:50:57 +0800

    update

--- 06-setup-repo.sh
+++ 06-setup-repo.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+if [ "${NO_ASK}" == "true" ];then
+  LOCAL_HTTP_PORT="8080"
+fi
+
+if [ -z "${LOCAL_HTTP_PORT}" ];then
+  read -r -p "Enter HTTP repository port [8080]: " input
+  LOCAL_HTTP_PORT=${input:-8080}
+fi
+
 yum install -y httpd
 sed -i 's/Listen 80$/Listen 8080/' /etc/httpd/conf/httpd.conf
-firewall-cmd --add-port=8080/tcp --zone=internal --permanent
-firewall-cmd --add-port=8080/tcp --zone=public   --permanent
+firewall-cmd --add-port="${LOCAL_HTTP_PORT}/tcp" --zone=internal --permanent
+firewall-cmd --add-port="${LOCAL_HTTP_PORT}/tcp" --zone=public --permanent
 firewall-cmd --reload
 systemctl start httpd
--- 09-remove-registry.sh
+++ 09-remove-registry.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-firewall-cmd --remove-port=5000/tcp --zone=internal --permanent
-firewall-cmd --remove-port=5000/tcp --zone=public   --permanent
+
+if [ -z "${LOCAL_REGISTRY_PORT}" ];then
+  read -r -p "Enter registry port [5000]: " input
+  LOCAL_REGISTRY_PORT=${input:-5000}
+fi
+
+firewall-cmd --remove-port="${LOCAL_REGISTRY_PORT}/tcp" --zone=internal --permanent
+firewall-cmd --remove-port="${LOCAL_REGISTRY_PORT}/tcp" --zone=public --permanent
 firewall-cmd --reload
 systemctl stop ocp4-registry.service
 systemctl disable ocp4-registry.service
